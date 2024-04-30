d445ef69ebb52b0176b207b5ad90f98034c16e5d
Kafrita-97 <despierta-tiempo-0c@icloud.com>
2024-04-30 05:44:12 +0200

    Initial Commit

---
+++ 80/_0App.swift
@@ -0,0 +1,17 @@
+//
+//  _0App.swift
+//  80
+//
+//  Created by Juanma on 30/4/24.
+//
+
+import SwiftUI
+
+@main
+struct _0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
